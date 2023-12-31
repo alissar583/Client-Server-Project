12420129b0f756007bcf964256739d01274099f5
Abdulrahem <abdulrahemalz1@gmail.com>
2023-12-31 18:26:14 +0300

    3

--- server_new.py
+++ server_new.py
@@ -7,6 +7,8 @@ import gnupg
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 def encrypt_session_key(session_key, public_key):
     encrypted_session_key = public_key.encrypt(
@@ -32,80 +34,24 @@ def receive_encrypted_session_key(ssl_socket):
     return encrypted_session_key
 
 
-# def decrypt_and_verify_session_key(encrypted_session_key, private_key):
-#     gpg_home = '/opt/homebrew/bin'  # Specify the path to the GPG executable
-#     gpg = gnupg.GPG(gnupghome=gpg_home)
-#     gpg = gnupg.GPG()
-#     gpg.import_keys(private_key)
-#     print(f"Private Key: {private_key}")
-#     print(f"Encrypted Session Key: {encrypted_session_key}")
-
-#     decrypted_data = gpg.decrypt(encrypted_session_key)
-
-#     print(f"Decryption Status: {decrypted_data.status}")
-#     print(f"Decrypted Data: {decrypted_data.data}")
-
-#     if not decrypted_data.ok:
-#         print(f"Decryption failed: {decrypted_data.status}")
-
-#     return decrypted_data.data
-
-# def decrypt_message(private_key, encrypted_message):
-#     decrypted_message = private_key.decrypt(
-#         encrypted_message,
-#         padding.OAEP(
-#             mgf=padding.MGF1(algorithm=hashes.SHA256()),
-#             algorithm=hashes.SHA256(),
-#             label=None
-#         )
-#     )
-#     return decrypted_message
-
-
-# def decrypt_session_key(encrypted_key, private_key):
-#     decrypted_key = private_key.decrypt(
-#         encrypted_key,
-#         padding.OAEP(
-#             mgf=padding.MGF1(algorithm=hashes.SHA256()),
-#             algorithm=hashes.SHA256(),
-#             label=None
-#         )
-#     )
-#     print('decrypted_key: ' ,decrypted_key)
-#     return decrypted_key
-
-
-
-# def decrypt_session_key(encrypted_key, private_key_bytes):
-    print('tesssty', encrypted_key)
-    private_key = serialization.load_pem_private_key(
-        private_key_bytes,
-        password=b'abd12',
-        backend=default_backend()
-    )
-    # print('private_keyL: ', private_key)
-    decrypted_key = private_key.decrypt(
-        encrypted_key,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-    print('decrypted_key: ', decrypted_key)
-    return decrypted_key
-
 
 def decrypt_session_key(encrypted_session_key, private_key):
-    session_key = private_key.decrypt(
-        encrypted_session_key,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
+    try:
+        session_key = private_key.decrypt(
+            encrypted_session_key,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
         )
-    )
-    return session_key
+        return session_key
+    # except InvalidSignature:
+    #     print("Decryption error: Invalid signature")
+    #     return None
+    except Exception as e:
+        print("Decryption error:", str(e))
+        return None
 
 
 def send_approval_to_client(ssl_socket):
@@ -130,9 +76,17 @@ def load_public_key_from_file(filename):
             )
         return key
 
+def decrypt(ciphertext, session_key):
+    iv = ciphertext[:16]  # Extract IV from the ciphertext
+    cipher = Cipher(algorithms.AES(session_key), modes.CFB(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    decrypted_message = decryptor.update(ciphertext[16:]) + decryptor.finalize()
+    return decrypted_message.decode()
+
+
 def create_tls_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 8378))
+    server_socket.bind(('localhost', 8598))
     server_socket.listen(1)
 
     print("Waiting for a connection...")
@@ -157,10 +111,8 @@ def create_tls_server():
 
     # Convert the loaded public key to a string
     # server_public_key_str = server_public_key.decode()
-
-    # print(f"Loaded Server Public Key:\n{server_public_key_str}")
-
-    # Send and receive data over the secure connection
+    user_name = ssl_socket.recv(1024).decode()
+    print("user_name: ", user_name)
     data = ssl_socket.recv(1024)
     print(f"Received: {data.decode()}")
     data = ssl_socket.recv(1024)
@@ -169,68 +121,22 @@ def create_tls_server():
     ssl_socket.send(server_public_key)
     encrypted_session_key = receive_encrypted_session_key(ssl_socket)
     print('encrypted_session_key: ', encrypted_session_key)
-    # with open('server_new_private_key.pem', 'rb') as key_file:
-    #     server_new_private_key = key_file.read()
-    server_new_private_key = load_private_key_from_file('test_private_key.pem')
+    private_key = f'private_keys/{user_name}_private_key.pem'
+    server_new_private_key = load_private_key_from_file(private_key)
     decrypted_session_key = decrypt_session_key(encrypted_session_key, server_new_private_key)
     print(f"Decrypted Session Key: {decrypted_session_key}")
-
+    send_approval_to_client(ssl_socket)
+    while True:
+        encrypted_message = ssl_socket.recv(1024)
+        # Decrypt message on the server side
+        decrypted_message = decrypt(encrypted_message, decrypted_session_key)
+        print("Decrypted Message on Server:", decrypted_message)
+  
     # Close the connection
     ssl_socket.close()
     server_socket.close()
 
 
-def generate_key_pair():
-    # Generate a new RSA private key
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,  # Commonly used value for the public exponent
-        key_size=2048,  # Key size in bits
-        backend=default_backend(),
-    )
-
-    # Get the public key from the private key
-    public_key = private_key.public_key()
-
-    # Serialize the private key to PEM format
-    private_key_pem = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption(),
-    )
-
-    # Serialize the public key to PEM format
-    public_key_pem = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo,
-    )
-
-    # Save the private key to a file
-    with open("server_private_key.pem", "wb") as private_key_file:
-        private_key_file.write(private_key_pem)
-
-    # Save the public key to a file
-    with open("server_public_key.pem", "wb") as public_key_file:
-        public_key_file.write(public_key_pem)
-
-
-def test():
-      # Load key pair
-    loaded_private_key = load_private_key_from_file('test_private_key.pem')
-    loaded_public_key = load_public_key_from_file('test_public_key.pem')
-
-    # Example session key
-    session_key = b'abdrere'
-
-    # Encrypt session key using the public key
-    encrypted_session_key = encrypt_session_key(session_key, loaded_public_key)
-    print(f'Encrypted Session Key: {encrypted_session_key}')
-    # encrypted_session_key = b'\xa1\x99k\x18\xe1\xe1\x1d\xaa\xe7t\xb5\x80\x97O\xe2@\xe4\xb1\xf8"\x91ha\x9d\xb9P\xba\n7\xa8\xc0\x8ds\x1f\x82\xd4\xac\'\x1f Z\x06\xacVB\x85\xec\xb3\x1e\x02O+G\xfc\xd2\\\xce\x9e\x03\xf2$!#\x7f\x0fB\x18LAC/\xf3\xe6\xc3U\x1d\xb5\xea\xf9\x1b\x1bs\xfb\xe2\x8b\xec\xbb\xf7\x99/\x1d\xa7\xfa\x84\xdc\xbf\x99\'\x16F.\x9a\x14\x1f\x88{\x14\xb4\xab\xb0k\xcb\xef{\xe9LMe\xf6\x83IZ\xc2\'\xe1\x83\xdc\xe23\xb2\x14\'\x88\x80\xb0\xc1"]\x03Y\x97A\xc94\x058M;Cs\x8b\'\x17*\xbe\x05\xc9\x92W5\xbc\x1f\xf5d\x13z\xe1\x96\xca\xce\x85\'\x92p<5\x9a\x14\xa2o\xa27\xa5\xea\xfa\x16\xb7N{\x80\x82;\x16\xcf\x89\x97Bs.\x04\x9c\xfc\xd1\xe4\x92\xfe\xf0(\xc3\x10\x04Q\x1aC\xae\xe5\x9d\x1ey\xb8~\xce\x7fB\x8f_w\xf1\xe1\xb1\xd0``\x11@h\xca\x1e\xfb\xb8<R\xf7\x18\xf5\x03\x18\x81(\xcd\x0c\xf6\xec\xda\xc9\xb6'
-    # Decrypt session key using the private key
-    decrypted_session_key = decrypt_session_key(encrypted_session_key, loaded_private_key)
-    print(f'Decrypted Session Key: {decrypted_session_key}')
-
 if __name__ == "__main__":
-    # generate_key_pair()
     create_tls_server()
-    # test()
   
